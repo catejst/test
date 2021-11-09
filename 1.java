804eb327b33b753933e9b81b2f1eb4b2c2afb98c
catejst <62929773+catejst@users.noreply.github.com>
2021-11-09 08:50:37 +0200

    Update 1.java

--- 1.java
+++ 1.java
@@ -3,3 +3,10 @@ SECRET_KEY = '%=*q42^m_mv271y6+q-+1md%93_h%d73f7^@()lkt^y!lt46'
 
 1/56991353579679:9cf301bfb55098d37f71e0c3bb764485
  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
+
+
+void main(){
+
+AID092L3M0
+bba2e32ed4f079d9c252ff102469b7af
+}
